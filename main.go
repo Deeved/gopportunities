923aa60670442333f90decb86745f5ac7aa4019f
Deeved Hiuston <deeved@gmail.com>
2024-07-08 22:56:48 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"github.com/Deeved/gopportunities/config"
 	"github.com/Deeved/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.NewLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
